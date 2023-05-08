50761f3f051057c5c598875bdec24195fd3fee1b
Larryking007 <ajikobilanre007@gmail.com>
2023-05-08 03:17:51 +0100

    Header and footer

---
+++ components/Hero.jsx
@@ -0,0 +1,18 @@
+import css from "../styles/Hero.module.css";
+import Image from "next/image";
+import Cherry from "../assets/Cherry.png";
+
+export default function Hero() {
+  return (
+    <div className={css.container}>
+      {/* left side */}
+      <div className={css.left}></div>
+      <div className={css.cherryDiv}>
+        <span>More than Faster</span>
+        <Image src={Cherry} alt="" width={40} height={25} />
+      </div>
+      {/* right side */}
+      <div className={css.rightSide}>1</div>
+    </div>
+  );
+}
